5231e529dc06d9a6d6269ba8600f5de31e80f7e4
Jesse Chen <jesse.z.chen@gmail.com>
2019-11-24 14:18:26 -0500

    Draw the map

--- bridgit.js
+++ bridgit.js
@@ -1,3 +1,5 @@
+const svg = document.getElementById('canvas');
+const svgNS = svg.namespaceURI;
 const map = [
 	[4, 0, 0, 5, 0, 0, 0, 4, 0, 3],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,3 +12,20 @@ const map = [
 	[0, 2, 0, 0, 0, 0, 0, 0, 0, 2],
 	[4, 0, 0, 0, 0, 0, 4, 0, 3, 0],
 ];
+for (let i = 0; i < 10; i++) {
+	for (let j = 0; j < 10; j++) {
+		if (map[j][i] !== 0) {
+			const circle = document.createElementNS(svgNS, 'circle');
+			circle.classList.add('island');
+			circle.setAttribute('cx', i * 100);
+			circle.setAttribute('cy', j * 100);
+			svg.appendChild(circle);
+			const label = document.createElementNS(svgNS, 'text');
+			label.innerHTML = map[j][i];
+			label.classList.add('label');
+			label.setAttribute('x', i * 100);
+			label.setAttribute('y', j * 100);
+			svg.appendChild(label);
+		}
+	}
+}
